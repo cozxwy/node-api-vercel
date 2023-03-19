afe922c3d695bb992bf8fa689a84fa9208344e7d
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-03-19 22:12:40 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -31,8 +31,16 @@ const API = new Blockfrost.BlockFrostAPI({
     
   });
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+
+
 app.listen(PORT , () => {
     console.log(`API litening on PORT ${PORT}`)
+    
 })
 
 
