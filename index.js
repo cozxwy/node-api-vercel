1ac59510a27f8eae36133dc6aac51e09cc79d27d
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-03-19 20:01:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+
+app.listen(PORT , () => {
+    console.log(`API litening on PORT ${PORT}`)
+})
+
+
+app.get('/' , (req,res) => {
+    res.send('this is my API running...')
+}) 
+
+app.get('/about' , (req,res) => {
+    res.send('this is my about route.')
+}) 
+
+
+module.exports = app
+
+
