d78199319e45e45d82ed00e3c9101fdbcf36f06b
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-03-20 16:52:21 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,35 +7,28 @@ const serviceAccount = require("./hippyhorse-9cfdb-firebase-adminsdk-2b9z2-615fc
 const cron = require('node-cron');
 
 
-admin.initializeApp({
-    credential: admin.credential.cert(process.env.firestore_key)
-}); 
-
-
-
-const db = admin.firestore();
 // import { BlockFrostAPI } from '@blockfrost/blockfrost-js'; // using import syntax
 
 
 
 const API = new Blockfrost.BlockFrostAPI({
-    //projectId: process.env.BlockFrostAPIKey, // see: https://blockfrost.io
-    projectId: process.env.BlockFrostAPIKeyMainnet, // see: https://blockfrost.io
-
-    
-  });
+  //projectId: process.env.BlockFrostAPIKey, // see: https://blockfrost.io
+  projectId: process.env.BlockFrostAPIKeyMainnet, // see: https://blockfrost.io
 
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+});
 
-app.listen(PORT , () => {
-    console.log(`API litening on PORT ${PORT}`)
-    
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`API litening on PORT ${PORT}`)
+
 })
 
 
@@ -62,31 +55,41 @@ app.listen(PORT , () => {
 }) */
 
 
-app.get('/getAssestByPolicyIdLoop' , async (req,res) => {
+app.get('/getAssestByPolicyIdLoop', async (req, res) => {
+
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+
+  admin.initializeApp({
+    credential: admin.credential.cert(process.env.firestore_key)
+  });
+
+
+
+  const db = admin.firestore();
+  
+
 
   cron.schedule('*/5 * * * *', () => {
     const docRef = db.collection('users').doc('alice');
 
     const data = {
-        name: 'AliceZ' + Math.floor(Math.random() * 112),
-        age: Math.floor(Math.random() * 99)
+      name: 'AliceZ' + Math.floor(Math.random() * 112),
+      age: Math.floor(Math.random() * 99)
     };
 
     docRef.update(data);
 
-});
+  });
 
-res.send('ok');
+  res.send('ok');
 
 
 
-}) 
+})
 
 
 
 module.exports = app
-
-
