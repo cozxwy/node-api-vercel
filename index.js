dde27763a7dc79288c67176be71174fdbe405055
cozxwy <96464326+cozxwy@users.noreply.github.com>
2023-03-20 19:39:01 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,58 +1,19 @@
 const express = require('express')
 const app = express()
 const PORT = 4000
-const Blockfrost = require("@blockfrost/blockfrost-js");
+
+app.listen(PORT , () => {
+  console.log(` API listenning at ${PORT}`)
+})
+
+app.get('/', (req,res) => {
+  res.send('this my api run')
+})
 
 
-
-// import { BlockFrostAPI } from '@blockfrost/blockfrost-js'; // using import syntax
-
-/*admin.initializeApp({
-  credential: admin.credential.cert(process.env.firestore_key)
-});
-const db = admin.firestore();
-*/
-
-
-const API = new Blockfrost.BlockFrostAPI({
-  //projectId: process.env.BlockFrostAPIKey, // see: https://blockfrost.io
-  projectId: process.env.BlockFrostAPIKeyMainnet, // see: https://blockfrost.io
-
-
-});
-
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
-
-/*app.get('/getAssestByPolicyId' , async (req,res) => {
-  
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    try {
-  
-    const latestBlock = await API.assetsPolicyByIdAll("583c9e403f5974a6a3a186972dabaacf2a759fa0913ed9f12b34164d")
-      console.log("latestBlock", latestBlock);
-      let a = JSON.stringify(latestBlock)
-      res.send(`${a}`)
- 
-    } catch (err) {
-      console.log("error", err);
-      res.send(`error  ${err}`)
-    }
-
-
-
-
-}) */
-
-
+app.get('/about', (req,res) => {
+  res.send('this my about')
+})
 
 
 module.exports = app
